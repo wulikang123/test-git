eefa670c8fc4373414a44be43cf8eb2c7977530b
wulikang <wulikang@qq.com>
2023-01-17 16:28:40 +0800

    first commit

---
+++ src/main/java/com/test/git/GitTest1.java
@@ -0,0 +1,12 @@
+package com.test.git;
+
+/**
+ * @author dongtangqiang
+ */
+public class GitTest1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("aaa");
+    }
+}
