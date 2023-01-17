198c72aea79bae9e63c1d33e6fc39e9f6214a6fe
wulikang <wulikang@qq.com>
2023-01-17 16:35:37 +0800

    second commit

--- src/main/java/com/test/git/GitTest1.java
+++ src/main/java/com/test/git/GitTest1.java
@@ -7,6 +7,6 @@ public class GitTest1 {
 
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("aaa");
+        System.out.println("hello git2!");
     }
 }
