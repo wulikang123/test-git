23fc3cd8002fd05bd58bc347e54722d20a0beb67
wulikang <wulikang@qq.com>
2023-01-17 16:38:18 +0800

    third commit

--- src/main/java/com/test/git/GitTest1.java
+++ src/main/java/com/test/git/GitTest1.java
@@ -8,5 +8,6 @@ public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
